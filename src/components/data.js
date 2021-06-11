eef47abaf8b6474b7a237f887c5443bfb4bf1df3
reham-salem <reham.salem6111@gmail.com>
2021-06-12 01:36:37 +0200

    codeing-data

--- src/components/data.js
+++ src/components/data.js
@@ -1,6 +1,214 @@
+import Footer from './footer';
+import DealSection from './deal-section';
 const Data = () => {
     return (
-     <div>data-accurasy</div>
+     <div >
+     <div className="Data-section">
+     <section className="row">
+         <h1 className="heading-one">Data accuracy</h1>
+         <p className="p-one">We combine data from over 15 reliable sources both commercial and public</p>
+         <p>Our goal is to constantly improve our data resolution to get to a point where each individual 
+            property can be accuratelly evaluated based on neighbourhood quality, investement potential and
+            value of the property itself
+        </p>
+     </section>
+     </div>
+     {/*            section of 2 tabel                              */}
+     <section className="row tabels">
+         {/*              tabel num 1                                      */}
+         <section className="col-lg-5   offset-lg-1 col-sm-10 offset-sm-2 ">
+            <h2 className="title">Property Market</h2>
+            <div className="bordertabel">
+            <table className="tabel">
+                <thead className="thead">
+                    <tr>
+                        <td>Metric</td>
+                        <td>Data Points</td>
+                        <td>Last Update</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr className="asking-price">
+                        <td className="td">Asking Price</td>
+                        <td>433.233</td>
+                        <td>43d ago</td>
+                    </tr>
+                    <tr className="rent">
+                        <td>Rent</td>
+                        <td>433.233</td>
+                        <td>43d ago</td>
+                    </tr>
+                    <tr className="asking-price">
+                        <td>Asking Price</td>
+                        <td>433.233</td>
+                        <td>43d ago</td>
+                    </tr>
+                    <tr className="rent">
+                        <td>Rent</td>
+                        <td>433.233</td>
+                        <td>43d ago</td>
+                    </tr>
+                    <tr className="asking-price">
+                        <td >Asking Price</td>
+                        <td>433.233</td>
+                        <td>43d ago</td>
+                    </tr>
+                    <tr className="rent">
+                        <td>Rent</td>
+                        <td>433.233</td>
+                        <td>43d ago</td>
+                    </tr>
+                    <tr className="asking-price">
+                        <td >Asking Price</td>
+                        <td>433.233</td>
+                        <td>43d ago</td>
+                    </tr>
+                    <tr className="rent">
+                        <td>Rent</td>
+                        <td>433.233</td>
+                        <td>43d ago</td>
+                    </tr>
+                    <tr className="asking-price">
+                        <td >Asking Price</td>
+                        <td>433.233</td>
+                        <td>43d ago</td>
+                    </tr>
+                    <tr className="rent">
+                        <td>Rent</td>
+                        <td>433.233</td>
+                        <td>43d ago</td>
+                    </tr>
+                    <tr className="asking-price">
+                        <td >Asking Price</td>
+                        <td>433.233</td>
+                        <td>43d ago</td>
+                    </tr>
+                    <tr className="rent">
+                        <td>Rent</td>
+                        <td>433.233</td>
+                        <td>43d ago</td>
+                    </tr>
+                    <tr className="asking-price">
+                        <td >Asking Price</td>
+                        <td>433.233</td>
+                        <td>43d ago</td>
+                    </tr>
+                    <tr className="rent">
+                        <td>Rent</td>
+                        <td>433.233</td>
+                        <td>43d ago</td>
+                    </tr>
+                    <tr className="asking-price">
+                        <td >Asking Price</td>
+                        <td>433.233</td>
+                        <td>43d ago</td>
+                    </tr>
+                </tbody>
+            </table>
+            </div>
+         </section>
+          {/*              tabel num 2                                      */}
+         <section className="col-lg-5   offset-lg-1  offset-sm-2 col-sm-10  ">
+            <h2 className="title mt-lg-0 mt-sm-3 ">Socio-economic</h2>
+            <div className="bordertabel">
+            <table className="tabel">
+                <thead className="thead">
+                    <tr>
+                        <td>Metric</td>
+                        <td>Data Points</td>
+                        <td>Last Update</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr className="asking-price">
+                        <td >Crime Rate</td>
+                        <td>street</td>
+                        <td>43d ago</td>
+                    </tr>
+                    <tr className="rent">
+                        <td>Occupations</td>
+                        <td>country</td>
+                        <td>43d ago</td>
+                    </tr>
+                    <tr className="asking-price">
+                        <td>Asking Price</td>
+                        <td>433.233</td>
+                        <td>43d ago</td>
+                    </tr>
+                    <tr className="rent">
+                        <td>Rent</td>
+                        <td>433.233</td>
+                        <td>43d ago</td>
+                    </tr>
+                    <tr className="asking-price">
+                        <td >Asking Price</td>
+                        <td>433.233</td>
+                        <td>43d ago</td>
+                    </tr>
+                    <tr className="rent">
+                        <td>Rent</td>
+                        <td>433.233</td>
+                        <td>43d ago</td>
+                    </tr>
+                    <tr className="asking-price">
+                        <td >Asking Price</td>
+                        <td>433.233</td>
+                        <td>43d ago</td>
+                    </tr>
+                    <tr className="rent">
+                        <td>Rent</td>
+                        <td>433.233</td>
+                        <td>43d ago</td>
+                    </tr>
+                    <tr className="asking-price">
+                        <td >Asking Price</td>
+                        <td>433.233</td>
+                        <td>43d ago</td>
+                    </tr>
+                    <tr className="rent">
+                        <td>Rent</td>
+                        <td>433.233</td>
+                        <td>43d ago</td>
+                    </tr>
+                    <tr className="asking-price">
+                        <td >Asking Price</td>
+                        <td>433.233</td>
+                        <td>43d ago</td>
+                    </tr>
+                    <tr className="rent">
+                        <td>Rent</td>
+                        <td>433.233</td>
+                        <td>43d ago</td>
+                    </tr>
+                    <tr className="asking-price">
+                        <td >Asking Price</td>
+                        <td>433.233</td>
+                        <td>43d ago</td>
+                    </tr>
+                    <tr className="rent">
+                        <td>Rent</td>
+                        <td>433.233</td>
+                        <td>43d ago</td>
+                    </tr>
+                    <tr className="asking-price">
+                        <td >Asking Price</td>
+                        <td>433.233</td>
+                        <td>43d ago</td>
+                    </tr>
+                </tbody>
+            </table>
+            </div>
+         </section>
+     </section>
+      {/*             section of dealSection component                                     */}
+     {/*  <section className="row">
+       <DealSection/>
+     </section> */}
+     {/*             section of footer component                                      */}
+     <section className="row">
+        <Footer/>
+     </section>
+     </div>
     );
   };
   
