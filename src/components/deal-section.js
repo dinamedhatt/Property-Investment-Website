d54c4948bd7e87f445537bbd0caa0c5abd952d6e
Dina Medhat <81185920+dinamedhatt@users.noreply.github.com>
2021-06-11 14:17:19 +0200

    Create deal-section.js

---
+++ src/components/deal-section.js
@@ -0,0 +1,9 @@
+const DealSection=()=>{
+    return (
+      <div className='' style={{"background-color":" rgba(243, 243, 243, 0.53)"}}>
+        deal section
+      </div>
+    )
+  }
+  
+  export default DealSection;
