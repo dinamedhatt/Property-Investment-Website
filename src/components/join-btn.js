8b24bb4386d069d2fdaf71fd61c4c42985905ad2
Dina Medhat <81185920+dinamedhatt@users.noreply.github.com>
2021-06-11 14:27:41 +0200

    Create join-btn.js

---
+++ src/components/join-btn.js
@@ -0,0 +1,7 @@
+const JoinBtn=()=>{
+    return (
+      <input class='btn btn-medium btn-rounded px-4' style={{"background-color":"#2b59b4", color:"white"}} type='button' value='Join the waitlist'/>
+    )
+  }
+  
+  export default JoinBtn;
