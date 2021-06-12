17774b48127c4e93e470c42739023b450ebb0d35
khloud fawzy <31770136+khloudfawzy@users.noreply.github.com>
2021-06-12 19:35:50 +0200

    Update feature-item.js

--- src/components/feature-item.js
+++ src/components/feature-item.js
@@ -3,18 +3,31 @@ import { Component } from "react";
 class FeatureItem extends Component {
   state = {
     feature: {
-      img: "./../../public/images/features/1.png",
-      //   img: "D:iti/0 Graduation ProjectProperty-Investment-Websitepublicimages/features/1.png",
+      //img: "../../public/images/features/1.png",
+      imgSrc: "1",
+      imgAlt: "hi",
+      heading: "Powerfull filters",
+      description:
+        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
     },
   };
-  //   isChecked = (event) => {
-  //     console.log(this.state.feature.img);
-  //   };
+  changeImg = () => {};
 
   render() {
     return (
       <section className="featureItem-section">
-        <img src={this.state.feature.img} alt="feature img 1" />
+       <div className="img-container">
+       <img
+          src={`/images/features/${this.state.feature.imgSrc}.png`}
+          //src="/images/features/1.png"
+          alt={this.state.feature.imgAlt}
+        />
+       </div>
+
+       <div className="featureText-container">
+        <h4>{this.state.feature.heading}</h4>
+        <p>{this.state.feature.description}</p>
+       </div>
       </section>
     );
   }
