94663e81e1ca5a999139a08e0fd0d9124fcafb1a
Dina Medhat <81185920+dinamedhatt@users.noreply.github.com>
2021-06-12 20:31:03 +0200

    Update retirement.js

--- src/components/retirement.js
+++ src/components/retirement.js
@@ -1,9 +1,8 @@
 import JoinBtn from '../containers/join-btn'
 const Retirement = () => {
     return (
-      <div className='py-5'>
-          <img src='images/retire.png' alt='retire' className='col-lg-3 col-md-4 d-sm-none d-md-block offset-1 position-relative' style={{zIndex:1}}/>
-          <div className='p-5 position-absolute' style={{backgroundColor:"#e8f0ff", top:"18%", zIndex:0,width:"100%"}}>
+      <div className='py-5 position-relative'>
+          <div className='p-5 position-absolute' style={{backgroundColor:"#e8f0ff", top:"22%",width:"100%", zIndex:0}}>
               <div className='col-lg-6 offset-lg-4 px-lg-5 col-md-5 offset-md-6 col-sm-12'>
               <h3>Thinking about early retirement?</h3>
               <p className='py-3'>Beat old brick & morter investors sparing yourself tons  of
@@ -11,6 +10,7 @@ research and acieving your goals earlier</p>
               <JoinBtn/>
               </div>
           </div>
+          <img src='images/retire.png' alt='retire' className='col-lg-3 col-md-4 d-sm-none d-md-block offset-1 position-relative' style={{zIndex:1}}/>
       </div>
     );
   };
