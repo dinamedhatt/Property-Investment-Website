b6562f6cf90d5216648cb342265656d0802cb55d
Dina Medhat <81185920+dinamedhatt@users.noreply.github.com>
2021-06-14 02:30:14 +0200

    Update error.js margins

--- src/components/error.js
+++ src/components/error.js
@@ -5,11 +5,11 @@ const history = useHistory();
 const reDirect = () => history.push('/'); 
     
 return(
-        <div className='text-center position-relative p-5'>
+        <div className='text-center position-relative p-5 my-4'>
             <h1 className='display-1 fw-bold position-absolute' style={{color:"#2b59b4",top:20,left:"52%"}}>404</h1>
             <h4 className='position-absolute px-4 py-2 bg-dark text-white' style={{top:"20%",left:"50%"}}>Page Not Found</h4>
             <img src='images/error.jpg' alt='error' className='img-fluid'/>
-            <button className='btn btn-medium btn-rounded px-4 d-block m-auto' style={{ backgroundColor: "#2b59b4", color: "white" }} onClick={reDirect}>Redirect to Home</button>
+            <button className='btn btn-medium btn-rounded px-4 d-block mx-auto mt-4' style={{ backgroundColor: "#2b59b4", color: "white" }} onClick={reDirect}>Redirect to Home</button>
         </div>
     )
 }
