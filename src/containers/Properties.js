6fc02d71fb35809b5fb21dceeff416c8ba5a948e
khloud fawzy <31770136+khloudfawzy@users.noreply.github.com>
2021-06-23 23:55:03 +0200

    updating property

--- src/containers/Properties.js
+++ src/containers/Properties.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { getproperties } from "../actions";
 import { BsSearch } from "react-icons/bs";
+import { FaBookmark, FaRegBookmark, FaCoins } from "react-icons/fa";
 import { IconContext } from "react-icons";
 
 class Properties extends Component {
@@ -15,7 +16,7 @@ class Properties extends Component {
 
   render() {
     return (
-      <div className="properties-container">
+      <div className="properties-container ">
         <div className="search-container">
           &nbsp;
           <div className="searchBar-container">
@@ -28,8 +29,12 @@ class Properties extends Component {
             <input type="button" value="find" className="properties-find-btn" />
           </div>
           <div className="filter-container">
-            <select name="investment type" id="investment-type">
-              <option value="investment-type" defaultValue disabled>
+            <select
+              name="investment type"
+              id="investment-type"
+              defaultValue="investment-type"
+            >
+              <option value="investment-type" disabled>
                 investment type
               </option>
               <option value="Sale">Sale</option>
@@ -37,8 +42,12 @@ class Properties extends Component {
               <option value="Partnership">Partnership</option>
             </select>
             {/*  */}
-            <select name="property type" id="property-type">
-              <option value="property-type" defaultValue disabled>
+            <select
+              name="property type"
+              id="property-type"
+              defaultValue="property-type"
+            >
+              <option value="property-type" disabled>
                 property type
               </option>
               <option value="Apartment">Apartment</option>
@@ -46,18 +55,18 @@ class Properties extends Component {
               <option value="Office">Office</option>
             </select>
             {/*  */}
-            <select name="budget" id="budget">
-              <option value="budget" defaultValue disabled>
+            <select name="budget" id="budget" defaultValue="budget">
+              <option value="budget" disabled>
                 budget
               </option>
-              <option value="volvo">1</option>
-              <option value="saab">2</option>
-              <option value="mercedes">3</option>
-              <option value="audi">4</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
             </select>
             {/*  */}
-            <select name="location" id="location">
-              <option value="location" defaultValue disabled>
+            <select name="location" id="location" defaultValue="location">
+              <option value="location" disabled>
                 location
               </option>
               <option value="England">England</option>
@@ -67,12 +76,57 @@ class Properties extends Component {
             </select>
           </div>
         </div>
+
+        <div className="property-items-container">
+          {this.state.propertiesList.map((property, key) => {
+            return (
+              <div className="property-item " key={key}>
+                <span className="icon-container">
+                  <IconContext.Provider
+                    value={{ className: "item-react-icons" }}
+                  >
+                    <FaRegBookmark />
+                  </IconContext.Provider>
+                </span>
+                <img
+                  src={`/images/properties/${property.image}`}
+                  alt="property"
+                />
+                <div className="property-item-info">
+                  <div>
+                    <h3>{property.name}</h3>
+                    <p>
+                      <IconContext.Provider
+                        value={{ className: "money-react-icons" }}
+                      >
+                        <FaCoins />
+                      </IconContext.Provider>
+                      {property.budget}
+                    </p>
+                  </div>
+                  <div>
+                    <p>{property.propType}</p>
+                    <input
+                      type="button"
+                      value="view"
+                      className="property-view-btn btn btn-outline-dark"
+                    />
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   }
   async componentDidMount() {
-    await this.props.getproperties();
-    this.setState({ propertiesList: this.props.propertieslist });
+    await this.props.getproperties(localStorage.getItem("jwt"));
+    // console.log(this.props.propertiesList);
+    // console.log(this.state.propertiesList);
+    // const arr = this.props.propertiesList;
+    this.setState({ propertiesList: this.props.propertiesList });
+    console.log(this.state.propertiesList);
   }
 }
 
