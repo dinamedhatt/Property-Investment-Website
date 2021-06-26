6e5660ec01fc6ee41eddee20d7280e2c793d9edc
Hana-Mahmoud <76908478+Hana-Mahmoud@users.noreply.github.com>
2021-06-27 01:07:40 +0200

    Update home.js

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -29,20 +29,20 @@ const Home = () => {
 
         <div
           id="animatio-one"
-          className="col-sm-3 row bg-white col-3  col-md-3 col-lg-3 rounded shadow  px-md-2 p-md-1 px-0 p-0 p-lg-0 m-0"
+          className="col-3 row bg-white  rounded shadow p-0 m-0"
           style={{
             height: "fit-content",
             position: "absolute",
-            top: "45%",
+            top: "44%",
             left: "20%",
           }}
         >
           <img
-            className="col-8 col-md-6 col-lg-5 col-xl-4 h-100 pt-3  pt-xl-2 img-fluid mt-lg-0 pt-lg-4 pt-md-3 ms-md-0 ms-sm-4 ms-3 pb-xxl-2"
+            className="col-8 col-md-5 col-lg-5 col-xl-4 h-100 pt-3  pt-xl-2 img-fluid mt-lg-0 pt-lg-2 pt-md-3 ms-md-0 ms-sm-4 ms-3 pb-xxl-2"
             src="images/home/1.png"
             alt="score"
           />
-          <p className="col-12 d-inline pt-3 fw-bold  fs-6 col-md-6 col-lg-7 col-xl-8 pt-lg-5 mt-lg-1 mt-xxl-3 pb-lg-3  pb-xl-2 pt-xl-4 text-center">
+          <p  className="col-12 d-inline pt-3 fw-bold  fs-6 col-md-7 col-lg-7 col-xl-8  pt-md-3 pt-lg-4 mt-lg-1 mt-xxl-3 pb-lg-3  pb-xl-2 pt-xl-4 text-center">
             Genie score
             <span className=" d-block fw-bold" style={{ fontSize: "1rem" }}>
               <span style={{ color: "#FFC107" }}>87</span> / 100
@@ -52,20 +52,20 @@ const Home = () => {
 
         <div
           id="animatio-two"
-          className="col-sm-3 row bg-white col-3  col-md-3 col-lg-3 rounded shadow  px-md-2 p-md-1 px-0 p-0 p-lg-0 m-0"
+          className="row bg-white col-3 rounded shadow p-0 m-0"
           style={{
             height: "fit-content",
             position: "absolute",
-            top: "70%",
+            top: "73%",
             right: "20%",
           }}
         >
           <img
-            className="col-8 col-md-6 col-lg-5 col-xl-4 h-100 pt-3  pt-xl-2 img-fluid mt-lg-0 pt-lg-4 pt-md-3 ms-md-0 ms-sm-4 ms-3 pb-xxl-2"
+            className="col-8  col-md-5  col-lg-5 col-xl-4 h-100 pt-3  pt-xl-2 img-fluid mt-lg-0 pt-lg-2 pt-md-3 ms-md-0 ms-sm-4 ms-3 pb-xxl-2"
             src="images/home/2.png"
             alt="roi"
           />
-          <p className="col-12 d-inline pt-3 fw-bold  fs-6 mt-lg-1 mt-xxl-3 col-md-6 col-lg-7 col-xl-8 pt-lg-5 pb-lg-3  pb-xl-2 pt-xl-4 text-center">
+          <p  className="col-12 d-inline pt-3 fw-bold  fs-6 mt-lg-1 mt-xxl-3 col-md-7 col-lg-7 col-xl-8 pt-lg-4 pb-lg-3 pt-md-3  pb-xl-2 pt-xl-4 text-center">
             Property ROI
             <span className=" d-block fw-bold" style={{ fontSize: "1rem" }}>
               <span style={{ color: "#FFC107" }}>8.93%</span>
