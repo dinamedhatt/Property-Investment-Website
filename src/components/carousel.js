b0e1d5b95a2a6e9d12dd824cc811daa84ae17002
Hana-Mahmoud <76908478+Hana-Mahmoud@users.noreply.github.com>
2021-06-27 00:10:23 +0200

    carousel updates

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -17,16 +17,17 @@ const settings = {
   }
     
     return(
-        <div className='py-5 row col-12' style={{position:"relative"}}>
+        <div className='py-5 m-0 row col-12' style={{position:"relative"}}>
             <h3 className='text-center mb-5'>Why Industry Leaders<br/>Choose Us?</h3>
 
-            <div className='bg-white row col-xxl-3 d-md-flex d-none  col-xl-4 col-lg-5 col-md-7  shadow rounded pb-xxl-4 pb-md-3' style={{position:"absolute",top:220, left:180, zIndex:1}}>
-                <img src='images/carousel/ceo.png' alt='ceo' className='  col-xxl-12 p-xxl-3  px-xl-5 pt-xl-2 offset-xl-0 pt-md-4 col-xl-12  col-lg-6  col-md-6 d-md-block d-none'/>
-                <p className='pt-3 d-md-block d-none lbl col-xl-12 col-6'>
+            <div  id="carouselPara" className='bg-white row col-xxl-3 col-xl-4 col-lg-5  m-lg-0 col-11 m-auto mb-3 shadow rounded pb-xxl-4 pb-lg-3 pb-lg-2 mt-xl-0 mt-lg-3 pb-md-3' >
+                <img src='images/carousel/ceo.png' alt='ceo' className='  col-xxl-12 p-xxl-3  px-xl-5 pt-xl-2 offset-xl-0 pt-md-4 col-xl-12  col-lg-6  col-md-6 '/>
+                <p className='pt-4  lbl col-xl-12 col-lg-6 col-md-5 ms-md-3 ms-lg-0 col-12'>
                 We believe now is always great time to be investing in property – regardless of short-term rises and drops, property is the single best asset class for returns!
                 <br/><br/>A wise man once said “Don’t wait to buy, buy then wait…”
+                <span className="text-secondary d-block pt-4">Mike Highnetworth</span>
                 </p><br/>
-                <p className='text-secondary col-xl-12 col-6  ms-xl-1 offset-md-6 d-md-block d-none  lbl pb-0 mb-0'>Mike Highnetworth</p>
+                
             </div>
 
             <Slider className='m-auto col-11 mb-5' {...settings}>{renderSlides()}</Slider>
